f4b8699f5f51c151cb83400804d695b4aef13fa5
keanii2018 <keani.mpcc@outlook.com>
2022-11-24 11:18:35 -0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Keani Antezana on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
